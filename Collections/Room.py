1c229359c1f38697b70b39c05feffe32a5428da1
Pyneni Chamanthi <54474521+Chamanthi17@users.noreply.github.com>
2025-04-21 18:55:12 -0500

    Room.py

--- Collections/Room.py
+++ Collections/Room.py
@@ -1,14 +1,17 @@
 from flask import request
-
+from mongoengine import *
 from Collections.Building import *
 
+# Blueprint for room endpoints
 room_endpoints = Blueprint('room_endpoints', __name__,
                            template_folder='templates')
-connect(host="mongodb://localhost:27017/student-dorm")
+
+# Connect to MongoDB Atlas (replace <your-username> and <your-password>)
+connect(host="mongodb+srv://<user>:<user>@cluster0.mwim6z3.mongodb.net/student-dorm?retryWrites=true&w=majority")
 
 is_setup_done = False
 
-
+# MongoEngine Document for Room
 class Room(Document):
     room_number = IntField()
     room_type = StringField()
@@ -18,7 +21,7 @@ class Room(Document):
     max_count = IntField()
     occupants = DictField()
 
-
+# Route to add a new room
 @room_endpoints.route("/room/add", methods=['POST'])
 def add_payment():
     if request.method == 'POST':
@@ -31,9 +34,9 @@ def add_payment():
         room.save()
         return str(room.id)
     else:
-        print('')
+        print('Invalid request method')
 
-
+# Function to get all vacant rooms
 def get_all_vacant_rooms():
     data = dict()
     data['2B'] = []
@@ -42,11 +45,11 @@ def get_all_vacant_rooms():
         data[room.room_type] += [room]
     return data
 
-
+# Function to get all rooms
 def get_all_rooms():
     return Room.objects()
 
-
+# Function to assign a room
 def assign_room(student_id, room_number, semester):
     room = Room.objects(room_number__exact=room_number)[0]
     room.occupants[semester].append(student_id)
@@ -55,7 +58,7 @@ def assign_room(student_id, room_number, semester):
     room.save()
     return room.price_per_semester
 
-
+# Setup data (rooms and buildings)
 def setup_data():
     male_building = Building(building_number='M1', gender='Male',
                              rooms_available=[101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
@@ -63,7 +66,7 @@ def setup_data():
     female_building = Building(building_number='F1', gender='Female',
                                rooms_available=[201, 202, 203, 204, 205, 206, 207, 208, 209, 210])
     female_building.save()
-    print('setup rooms data')
+    print('Setup rooms data')
     for building in Building.objects():
         start = 101 if building.building_number == 'M1' else 201
         for room_number in range(start, start + 10, 1):
