9e497d23a827ffa1b25a8b71b502f031aa2b54d2
Pyneni Chamanthi <54474521+Chamanthi17@users.noreply.github.com>
2025-04-21 18:51:45 -0500

    Admin.py

--- Collections/Admin.py
+++ Collections/Admin.py
@@ -1,26 +1,34 @@
 from flask import request, Blueprint
 from mongoengine import *
 
+# Set up Flask Blueprint
 admin_endpoints = Blueprint('admin_endpoints', __name__,
                             template_folder='templates')
-connect(host="mongodb://localhost:27017/student-dorm")
 
+# MongoDB Atlas connection string (replace <username>, <password>, and <dbname>)
+connect(host="mongodb+srv://<your-username>:<your-password>@cluster0.mwim6z3.mongodb.net/student-dorm?retryWrites=true&w=majority")
 
+# MongoEngine Document (Admin model)
 class Admin(Document):
     admin_id = StringField()
     name = StringField()
     email = StringField()
     password = StringField()
 
-
+# Route to add a new admin
 @admin_endpoints.route("/admin/add", methods=['POST'])
 def add_user():
     if request.method == 'POST':
+        # Get form data and create a new Admin object
         admin = Admin(admin_id=request.form.get('name'),
                       name=request.form.get('name'),
                       email=request.form.get('email'),
                       password=request.form.get('password'))
+        
+        # Save the Admin object to the database
         admin.save()
+        
+        # Return the Admin ID
         return str(admin.id)
     else:
-        print('')
+        print('Invalid request method')
