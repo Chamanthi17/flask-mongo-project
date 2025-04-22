dbd4c56296df758fa7794b3a634991591bf67017
Pyneni Chamanthi <54474521+Chamanthi17@users.noreply.github.com>
2025-04-21 20:35:42 -0500

    Update Building.py

--- Collections/Building.py
+++ Collections/Building.py
@@ -6,7 +6,7 @@ building_endpoints = Blueprint('building_endpoints', __name__,
                                template_folder='templates')
 
 # Connect to MongoDB Atlas using your connection string
-connect(host="mongodb+srv://user:user@cluster0.mwim6z3.mongodb.net/?retryWrites=true&w=majority")
+connect(host="mongodb+srv://user:user@cluster0.mwim6z3.mongodb.net/student-dorm?retryWrites=true&w=majority")
 
 # MongoEngine Document for Building
 class Building(Document):
